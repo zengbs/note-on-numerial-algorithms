8fd9f361749c231f3eb7d4a0fbaa31d70fec5fd6
Po-Hsun Tseng <zengbs@gmail.com>
2023-12-25 12:34:50 +0800

    Update

--- fft/dft__leakage.py
+++ fft/dft__leakage.py
@@ -18,7 +18,7 @@ original_signal = 1/np.sqrt(2*np.pi)/sigma * np.exp(-0.5*((t-mu)/sigma)**2)
 # Rectangular window function
 window = np.ones_like(t)
 window_width = 0.2
-window[:int((0.5-window_width/2)*len(t))] = 0
+window[:int((0.5-window_width/2)*len(t)) ] = 0
 window[ int((0.5+window_width/2)*len(t)):] = 0
 
 # Signal multiplied by the window function
@@ -36,13 +36,13 @@ freq = np.linspace(0, fs, len(t))
 fig, axs = plt.subplots(3, 2, figsize=(12, 8))
 
 # Plotting the original signal in time and frequency domain
-axs[0, 0].plot(t, original_signal, linestyle=":", color='blue')
+axs[0, 0].plot(t, original_signal, linestyle="-", color='blue', marker='o', label='line with marker', ms=3)
 axs[0, 0].set_title("Original Signal (Time Domain)")
 axs[0, 0].set_xlabel("Time")
 axs[0, 0].set_ylabel("$s$")
 axs[0, 0].set_xlim(min(t), max(t))
 
-axs[0, 1].plot(freq, np.abs(fft_original), linestyle=":", color='red')
+axs[0, 1].plot(freq, np.abs(fft_original), linestyle="-", color='red', marker='o', label='line with marker', ms=3)
 axs[0, 1].set_title("Original Signal (Frequency Domain)")
 axs[0, 1].set_xlabel("Frequency")
 axs[0, 1].set_ylabel(r"$|F(s)|$")
@@ -50,13 +50,13 @@ axs[0, 1].set_xlim(min(freq), max(freq))
 axs[0, 1].set_yscale('log')
 
 # Plotting the window function in time and frequency domain
-axs[1, 0].plot(t, window, linestyle=":", color='blue')
+axs[1, 0].plot(t, window, linestyle="-", color='blue', marker='o', label='line with marker', ms=3)
 axs[1, 0].set_title("Rectangular Window (Time Domain)")
 axs[1, 0].set_xlabel("Time")
 axs[1, 0].set_ylabel("$s$")
 axs[1, 0].set_xlim(min(t), max(t))
 
-axs[1, 1].plot(freq, np.abs(fft_window), linestyle=":", color='red')
+axs[1, 1].plot(freq, np.abs(fft_window), linestyle="-", color='red', marker='o', label='line with marker', ms=3)
 axs[1, 1].set_title("Rectangular Window (Frequency Domain)")
 axs[1, 1].set_xlabel("Frequency")
 axs[1, 1].set_ylabel(r"$|F(s)|$")
@@ -65,13 +65,13 @@ axs[1, 1].set_yscale('log')
 
 
 # Plotting the windowed signal in time and frequency domain
-axs[2, 0].plot(t, windowed_signal, linestyle=":", color='blue')
+axs[2, 0].plot(t, windowed_signal, linestyle="-", color='blue', marker='o', label='line with marker', ms=3)
 axs[2, 0].set_title("Windowed Signal (Time Domain)")
 axs[2, 0].set_xlabel("Time")
 axs[2, 0].set_ylabel("$s$")
 axs[2, 0].set_xlim(min(t), max(t))
 
-axs[2, 1].plot(freq, np.abs(fft_windowed_signal), linestyle=":", color='red')
+axs[2, 1].plot(freq, np.abs(fft_windowed_signal), linestyle="-", color='red', marker='o', label='line with marker', ms=3)
 axs[2, 1].set_title("Windowed Signal (Frequency Domain)")
 axs[2, 1].set_xlabel("Frequency")
 axs[2, 1].set_ylabel(r"$|F(s)|$")
